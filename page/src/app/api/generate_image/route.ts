42d950c887acc61e613c6c41545400ba82339798
Andrew Woo <awoo32@gatech.edu>
2024-01-26 16:52:53 -0500

    Update Image generate prompt

--- page/src/app/api/generate_image/route.ts
+++ page/src/app/api/generate_image/route.ts
@@ -8,10 +8,22 @@ export async function POST(req: Request) {
 
   const { key, element } = await req.json();
 
-  let prompt = `I NEED to test how the tool works with extremely simple prompts. DO NOT add any detail, just use it AS-IS: ${key}: ${element}`
+  let prompt = `Create an AI Image prompt for the ${key}: ${element}. Make sure it is less than 50 words.`;
+
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "user", content: prompt }],
+    model: "gpt-3.5-turbo",
+  });
+
+  let imgPromptRaw = completion.choices[0].message.content;
+  let imgPrompt = `I NEED to test how the tool works with extremely simple prompts. DO NOT add any detail, just use it AS-IS: ${key}: ${element}`;
+
+  if (imgPromptRaw !== null) {
+    imgPrompt = imgPromptRaw;
+  }
 
   const image = await openai.images.generate({ 
-    prompt: prompt,
+    prompt: imgPrompt,
     size: "256x256"
   });
 
