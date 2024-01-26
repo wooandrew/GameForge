68794f38b7143ab385ccfe74fa2eef753416e7cc
Andrew Woo <awoo32@gatech.edu>
2024-01-26 13:28:32 -0500

    Abandon GitHub pages deployment, switch to Vercel

--- page/next.config.mjs
+++ page/next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/GameForge'
-}
+const nextConfig = {}
 
-export default nextConfig;
+module.exports = nextConfig
